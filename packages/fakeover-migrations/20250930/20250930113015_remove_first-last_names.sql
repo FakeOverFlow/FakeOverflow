08b877bb0cd10ecd257971df886248f3d0d8b2cb
AlenGeoAlex <71920951+AlenGeoAlex@users.noreply.github.com>
2025-09-30 11:35:27 +0000

    chore(migrations): add incremental and idempotent SQL scripts for: 20250930113015_remove_first-last_names (in 20250930)
    - chore: update `VectorText` configuration in migration file (20250930113015_remove_first-last_names.cs)
    
      - Dropped and re-added `VectorText` column in `UserAccounts` with updated properties.
    - Refined `Npgsql:TsVectorProperties` to remove references to `FirstName` and `LastName`.
    
    Generated on: 20250930

---
+++ packages/fakeover-migrations/20250930/20250930113015_remove_first-last_names.sql
@@ -0,0 +1,14 @@
+﻿START TRANSACTION;
+ALTER TABLE public."UserAccounts" DROP COLUMN "FirstName";
+
+ALTER TABLE public."UserAccounts" DROP COLUMN "LastName";
+
+ALTER TABLE public."UserAccounts" DROP COLUMN "VectorText";
+
+ALTER TABLE public."UserAccounts" ADD "VectorText" tsvector GENERATED ALWAYS AS (to_tsvector('english', "Email" || ' ' || "Username")) STORED NOT NULL;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250930113015_remove_first-last_names', '9.0.9');
+
+COMMIT;
+

26f34c89b5b931635f085b78cb4eaf8f51bc3389
Jobit Jose <79743389+jobitjose@users.noreply.github.com>
2025-11-16 21:39:43 +0000

    created shared community stats and trending tags

---
+++ apps/fakeoverflow-angular/src/app/shared/trending-tags/trending-tags.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-tags',
+  standalone: true,
+  templateUrl: './trending-tags.html'
+})
+export class TrendingTags {
+
+}

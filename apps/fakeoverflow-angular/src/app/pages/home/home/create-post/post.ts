d33b8525e9a0b76147f9607015c279bedd497dac
Jobit Jose <79743389+jobitjose@users.noreply.github.com>
2025-11-18 19:13:54 +0000

    remodified the post page

--- apps/fakeoverflow-angular/src/app/pages/home/home/create-post/post.ts
+++ apps/fakeoverflow-angular/src/app/pages/home/home/create-post/post.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import {FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Navbar} from '@shared/navbar/navbar';
 import {PostService} from '../../../../../../../../packages/fakeoverflow-angular-services';
+import {CommunityStats} from '@shared/community-stats/community-stats';
+import {TrendingTags} from '@shared/trending-tags/trending-tags';
+import {RouterLink} from '@angular/router';
 
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [Navbar, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [Navbar, CommonModule, FormsModule, ReactiveFormsModule, CommunityStats, TrendingTags, RouterLink],
   templateUrl: './post.html',
   styleUrl: './post.scss'
 })
@@ -32,13 +35,19 @@ export class PostComponent {
       .subscribe({
         next: (response) => {
           console.log(response);
+          this.submitted = true;
+          this.loading = false;
+          this.formGroup.reset();
+
+          // Hide success message after 3 seconds
+          setTimeout(() => {
+            this.submitted = false;
+          }, 3000);
         },
         error: (err) => {
           console.error(err);
+          this.loading = false;
         }
       })
-    this.submitted = true;
-    this.loading = false;
-    this.formGroup.reset();
   }
 }

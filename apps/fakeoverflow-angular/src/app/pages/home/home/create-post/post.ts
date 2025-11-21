470f57441999403e335925add08f505ef1f4d71d
Alen Alex <alengeoalex@gmail.com>
2025-11-21 11:32:20 +0000

    Fixed create-post endpoint

--- apps/fakeoverflow-angular/src/app/pages/home/home/create-post/post.ts
+++ apps/fakeoverflow-angular/src/app/pages/home/home/create-post/post.ts
@@ -2,10 +2,13 @@ import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Navbar} from '@shared/navbar/navbar';
-import {PostService} from '../../../../../../../../packages/fakeoverflow-angular-services';
+import {
+  FakeoverFlowBackendHttpApiFeaturesPostsCreatePostsPostsRequest,
+  PostService
+} from '../../../../../../../../packages/fakeoverflow-angular-services';
 import {CommunityStats} from '@shared/community-stats/community-stats';
 import {TrendingTags} from '@shared/trending-tags/trending-tags';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 
 
 @Component({
@@ -18,10 +21,12 @@ import {RouterLink} from '@angular/router';
 export class PostComponent {
 
   private readonly postService = inject(PostService)
+  private readonly router = inject(Router)
 
   protected readonly formGroup = new FormGroup({
-    title: new FormControl(null, [Validators.required]),
-    content: new FormControl(null, [Validators.required]),
+    title: new FormControl('', [Validators.required]),
+    content: new FormControl('', [Validators.required]),
+    tags: new FormControl([], [])
   })
 
   loading = false;
@@ -29,17 +34,18 @@ export class PostComponent {
 
   submit() {
     if (this.formGroup.invalid) return;
-    console.log(this.formGroup.value);
+    const request : FakeoverFlowBackendHttpApiFeaturesPostsCreatePostsPostsRequest = this.formGroup.value! as FakeoverFlowBackendHttpApiFeaturesPostsCreatePostsPostsRequest;
     this.loading = true;
-    this.postService.createPost(this.formGroup.value as any)
+    this.postService.createPost(request)
       .subscribe({
         next: (response) => {
           console.log(response);
           this.submitted = true;
           this.loading = false;
           this.formGroup.reset();
+          this.router.navigate(['/post', response.id!])
+            .catch(console.error)
 
-          // Hide success message after 3 seconds
           setTimeout(() => {
             this.submitted = false;
           }, 3000);

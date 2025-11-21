4fff1ab64f22c210634423d8f5203f7168109174
Alen Alex <alengeoalex@gmail.com>
2025-11-21 11:09:36 +0000

    completed list post view card

--- apps/fakeoverflow-angular/src/app/pages/home/home/home.ts
+++ apps/fakeoverflow-angular/src/app/pages/home/home/home.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import { Navbar } from '@shared/navbar/navbar';
 import { RouterLink } from '@angular/router';
 import { CommunityStats } from '@shared/community-stats/community-stats';
 import { TrendingTags } from '@shared/trending-tags/trending-tags';
 
+import {PostListCard} from '@shared/posts/post-list-card/post-list-card';
+
+import {PostService, FakeoverFlowBackendHttpApiFeaturesPostsListPostsListPostsPostSummary} from '../../../../../../../packages/fakeoverflow-angular-services';
+
 @Component({
   selector: 'app-home',
   imports: [
-    Navbar, RouterLink, CommunityStats, TrendingTags
+    Navbar, RouterLink, CommunityStats, TrendingTags, PostListCard
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home implements OnInit{
 
+  protected readonly postService = inject(PostService);
+  protected posts: FakeoverFlowBackendHttpApiFeaturesPostsListPostsListPostsPostSummary[] = [];
+
+  ngOnInit(): void {
+        this.postService.listPosts()
+          .subscribe({
+            next: (response) => {
+              this.posts = response.posts || [];
+            },
+            error: (err) => {
+              console.error(err);
+            }
+          })
+    }
 }
---
+++ apps/fakeoverflow-angular/src/app/shared/posts/post-list-card/post-list-card.ts
@@ -0,0 +1,26 @@
+import {Component, inject, input, OnInit} from '@angular/core';
+import {
+  FakeoverFlowBackendHttpApiFeaturesPostsListPostsListPostsPostSummary,
+  PostService
+} from '../../../../../../../packages/fakeoverflow-angular-services';
+import {RelativeTimePipe} from '@pipes/relative-time-pipe';
+import {UserNameExtractorPipe} from '@pipes/user-name-extractor-pipe';
+
+@Component({
+  selector: 'app-post-list-card',
+  imports: [
+    RelativeTimePipe,
+    UserNameExtractorPipe
+  ],
+  templateUrl: './post-list-card.html',
+  styleUrl: './post-list-card.scss'
+})
+export class PostListCard implements OnInit{
+
+  readonly post = input.required<FakeoverFlowBackendHttpApiFeaturesPostsListPostsListPostsPostSummary>();
+
+  ngOnInit(): void {
+
+  }
+
+}

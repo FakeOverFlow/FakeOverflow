7d42d493f954e547a11f0769acb9def612aecb91
Jobit Jose <79743389+jobitjose@users.noreply.github.com>
2025-11-22 17:16:22 +0000

    view-questions

--- apps/fakeoverflow-angular/src/app/pages/home/home/view-question/view-question.ts
+++ apps/fakeoverflow-angular/src/app/pages/home/home/view-question/view-question.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Navbar } from '@shared/navbar/navbar';
+import { CommunityStats } from '@shared/community-stats/community-stats';
+import { TrendingTags } from '@shared/trending-tags/trending-tags';
 
 @Component({
   selector: 'app-view-question',
   standalone: true,
-  imports: [CommonModule, Navbar],
+  imports: [CommonModule, RouterLink, Navbar, CommunityStats, TrendingTags],
   templateUrl: './view-question.html',
   styleUrls: []
 })
